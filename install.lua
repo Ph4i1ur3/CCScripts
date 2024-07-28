4b63bc37cceec2284b81261d53e0719091c5a0a9
Xeane <Ph4i1ur3@yahoo.com>
2024-07-28 16:31:59 -0400

    update

--- install.lua
+++ install.lua
@@ -1,9 +1,9 @@
-makeDir("/github")
+fs.makeDir("github")
 
 local request = http.get("https://github.com/Ph4i1ur3/CCScripts/masterlist.txt")
 for file in request.readAll():gmatch("%S+") do
   local thisReq = http.get("https://github.com/Ph4i1ur3/CCScripts/scripts/" .. file)
-  local localFile = open("/github/" .. file, "w+")
+  local localFile = fs.open("github/" .. file, "w+")
   localFile.write(thisReq.readAll())
   localFile.close()
   thisReq.close()
