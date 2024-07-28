ab73a686554f75b5f8a2821cbf8547862d1e2f7a
Xeane <Ph4i1ur3@yahoo.com>
2024-07-28 17:05:47 -0400

    fixes

--- install.lua
+++ install.lua
@@ -11,7 +11,10 @@ print("Request Made.")
 for file in request.readAll():gmatch("%S+") do
   print(file .. " File Requested.")
   local thisReq = http.get("https://github.com/Ph4i1ur3/CCScripts/scripts/" .. file)
-  local localFile = fs.open("github/" .. file, "w+")
+  if fs.exists("github/" .. file) then
+    fs.delete("github/" .. file)
+  end
+  local localFile = fs.open("github/" .. file, "w")
   localFile.write(thisReq.readAll())
   localFile.close()
   thisReq.close()
